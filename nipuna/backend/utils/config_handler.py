ec09d079e81e960ce197c87af8cdc359ced541a2
JayReddy <nipunalabs@gmail.com>
2025-02-15 16:55:15 +0530

    Python version bumped down and added ollama chat functionality

--- nipuna/backend/utils/config_handler.py
+++ nipuna/backend/utils/config_handler.py
@@ -1,30 +1,33 @@
 from yaml import safe_load, dump
 import os
+import ast
 
 class YAMLHandler:
-    def __init__(self):
-        pass
+    def __init__(self, project_name):
+        self.project_name = project_name
 
-    def read_yaml_configs(self, project_name):
-        yaml_config_file = os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))),"utils/samples/sample.yaml")
+    def read_yaml_configs(self, yaml_config_file: str = None ):
+        if not yaml_config_file:
+            yaml_config_file = os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))),"utils/samples/sample.yaml")
 
         open_yaml = open(yaml_config_file, "r")
         parse_yaml = safe_load(open_yaml)
-        parse_yaml['project_name'] = project_name
+        parse_yaml['project_name'] = self.project_name
         return parse_yaml
     
-    def generate_assets(self, project_name):
-        yaml_payload = self.read_yaml_configs(project_name)
+    def generate_assets(self):
+        yaml_payload = self.read_yaml_configs()
 
         try:
-            project_path = os.path.join(os.getcwd(), project_name)
+            project_path = os.path.join(os.getcwd(), self.project_name)
             print(project_path)
             if not os.path.exists(project_path):
                 os.mkdir(project_path)
                 os.chdir(project_path)
-                with open('{}.yaml'.format(project_name), 'w') as file:
+                
+                with open('{}.yaml'.format(self.project_name), 'w') as file:
                     dump(yaml_payload, file)
         except:
             print("Please pass valid project name in config")
-        return "Generated Protect Utils"
+        return yaml_payload
     
--- nipuna/backend/utils/model_handler.py
+++ nipuna/backend/utils/model_handler.py
@@ -1,5 +1,57 @@
 
+from ollama import chat, Client
+from .config_handler import YAMLHandler
 
 class ModelHandler:
-    def __init__(self):
-        pass
+    def __init__(self, project_name: str, file_path: str):
+        self.project_name = project_name
+        self.file_path = file_path
+        self.handler = YAMLHandler(project_name=self.project_name)
+        self.configs = self.handler.read_yaml_configs(self.file_path)
+
+    def model_init(self, model_name: str):
+        # print(self.configs.keys())
+        model_dict = {"ollama": OllamaHandler(model_name=model_name)}
+        return model_dict
+        
+        # client, model_name,client_type, system = self.configs['custom_ollama'].values()
+        # model_dict = {"ollama": OllamaHandler(model_name=model_name)}
+        
+        # model = model_dict[client]
+        # model_ref = model.connect(client_type=client_type, system=system)
+        # print(model_ref)
+        # return model_ref
+    
+    def chat(self):
+        workflow_configs = self.configs['WORKFLOW']
+        client, model_name,role, content = self.configs[workflow_configs['model']].values()
+        model_dict = self.model_init(model_name=model_name)
+        model = model_dict[client]
+        model_resp = model.chat_with_model(role=role, content=content)
+        print(model_resp)
+
+class OllamaHandler:
+    def __init__(self, model_name: str):
+        self.model_name = model_name
+
+    def connect_custom(self, client_type: str, system: str, enable_streaming: bool = False,port: str = 11434):
+        model_client = Client(host='https://localhost:{}'.format(port))
+        model_ref = model_client.create(
+            model = client_type,
+            from_=self.model_name,
+            system=system,
+            stream=enable_streaming
+        )
+        return model_ref
+    
+    def chat_with_model(self, content: str, role: str):
+        print("Generating response...")
+        messages = [
+            {
+                'role': role,
+                'content': content
+            }
+        ]
+
+        chat_resp = chat(model=self.model_name, messages=messages)
+        return chat_resp
